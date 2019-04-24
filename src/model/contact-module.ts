53dbb7d41a0cc4b8a03c71c9e2ed94dfa28ccc00
elyyps <ahmed@webbio.nl>
2019-04-24 12:26:07 +0200

    feat: merge

--- src/model/contact-module.ts
+++ src/model/contact-module.ts
@@ -37,19 +37,41 @@ export class ContactModule extends WordPressPostModule {
 
 export const generateDummyContactModule = (): ContactModule => {
   return {
-     title:"Neem gelijk contact op",
-     description:"Aenean id lorem eleifend, malesuada arcu a, fringilla mauris. Pellentesque dolor lacus, luctus lobortis rhoncus vitae, ultricies vitae justo. Mauris ut interdum purus.",
-     subtitleContactAddress:"Contact adres",
-     contactAddressInfo:{shopName:"TerraSana",street:"Waaier 10", postalCode:"2451VV",city:"LEIMUIDEN"},
-     mailingAddress:"Postadres",
-     mailingAddressInfo:{shopName:"TerraSana" ,street:"Postbus 70", postalCode:"2450 AB" , city:"LEIMUIDEN"},
-     phone:"Telefonisch",
-     generalAndSales:{contactName:"Algemeen & verkoop",phone:"+31 (0) 172 - 50 33 33"},
-     marketing:{contactName:"Marketing",phone:"+31 (0) 172 - 50 33 47"}, 
-     customerService:{contactName:"Consumentenservice",phone:"+31 (0) 172 - 50 33 44"},
-     fax:{contactName:"Fax",phone:"+31 (0) 172 - 50 33 55"},
-     directContact:"DIRECT CONTACT",
-     contactForm:{email:"email",name:"name",privacyText:"Wanneer je op verzenden drukt ga je akkoord met ons",privacyLink:"/privacy"},
+    title: "Neem gelijk contact op",
+    description:
+      "Aenean id lorem eleifend, malesuada arcu a, fringilla mauris. Pellentesque dolor lacus, luctus lobortis rhoncus vitae, ultricies vitae justo. Mauris ut interdum purus.",
+    subtitleContactAddress: "Contact adres",
+    contactAddressInfo: {
+      shopName: "TerraSana",
+      street: "Waaier 10",
+      postalCode: "2451VV",
+      city: "LEIMUIDEN"
+    },
+    mailingAddress: "Postadres",
+    mailingAddressInfo: {
+      shopName: "TerraSana",
+      street: "Postbus 70",
+      postalCode: "2450 AB",
+      city: "LEIMUIDEN"
+    },
+    phone: "Telefonisch",
+    generalAndSales: {
+      contactName: "Algemeen & verkoop",
+      phone: "+31 (0) 172 - 50 33 33"
+    },
+    marketing: { contactName: "Marketing", phone: "+31 (0) 172 - 50 33 47" },
+    customerService: {
+      contactName: "Consumentenservice",
+      phone: "+31 (0) 172 - 50 33 44"
+    },
+    fax: { contactName: "Fax", phone: "+31 (0) 172 - 50 33 55" },
+    directContact: "DIRECT CONTACT",
+    contactForm: {
+      email: "email",
+      name: "name",
+      privacyText: "Wanneer je op verzenden drukt ga je akkoord met ons",
+      privacyLink: "/privacy"
+    },
 
     id: uuid(),
     topMargin: "",
