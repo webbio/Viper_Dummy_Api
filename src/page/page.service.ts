ed851685c2aeb3e1e692d40a991fce39d4d4c9ba
Tom Klein <tom@webbio.nl>
2019-04-26 14:10:12 +0200

    page setup sprint demo

--- src/page/page.service.ts
+++ src/page/page.service.ts
@@ -2,38 +2,18 @@ import { Injectable } from "@nestjs/common";
 import { PageModel } from "./page.model";
 import * as uuid from "uuid";
 import * as _ from "lodash";
-import {
-  WelcomeModule,
-  generateDummyWelcomeModule
-} from "src/model/welcome-module";
-import {
-  NavBarModule,
-  generateDummyNavBarModule
-} from "src/model/nav-bar-module";
+import { WelcomeModule, generateDummyWelcomeModule } from "src/model/welcome-module";
+import { NavBarModule, generateDummyNavBarModule } from "src/model/nav-bar-module";
 import { generateDummyCTAModule } from "src/model/cta-module";
 import { generateDummyInfoHeaderModule } from "src/model/info-header-module";
 import { generateBodyTextModule } from "src/model/bodytext-module";
 import { generateDummyFooterModule } from "src/model/footer-component";
-<<<<<<< HEAD
 import { generateDummySocialMediaButtons } from "src/model/social-media-buttons";
 import { OurTeamModule } from "src/model/ourTeam-module";
-import {
-  generateDummyContactModule,
-  ContactModule
-} from "src/model/contact-module";
+import { generateDummyContactModule, ContactModule } from "src/model/contact-module";
 import { generateDummyOurTeamModule } from "src/model/ourTeam-module";
-import {
-  generateDummySalePointModule,
-  SalePointModule
-} from "src/model/sale-point-module";
-import {
-  generateBodyTextExpandedModule,
-  BodyTextExpandedModule
-} from "src/model/bodytext-expanded-module";
-=======
-import { generateDummySocialMediaButtons } from "src/model/socialMediaButtons";
-import { generateDummyInstagramWallModule, InstagramWalllModule } from "src/model/instagram-wall";
->>>>>>> dev_daniele
+import { generateDummySalePointModule, SalePointModule } from "src/model/sale-point-module";
+import { generateBodyTextExpandedModule, BodyTextExpandedModule } from "src/model/bodytext-expanded-module";
 
 @Injectable()
 export class PageService {
@@ -49,37 +29,23 @@ export class PageService {
         generateDummyInfoHeaderModule(),
         generateDummyCTAModule(),
         generateDummyFooterModule(),
-<<<<<<< HEAD
         generateDummySalePointModule() as SalePointModule,
         generateDummyOurTeamModule() as OurTeamModule,
         generateDummyContactModule() as ContactModule
         // generateDummyProductOverviewModule() as ProductOverviewModule,
-=======
-        generateDummyInstagramWallModule() as InstagramWalllModule
->>>>>>> dev_daniele
       ]
     } as PageModel,
     {
       id: uuid(),
       route: "https://terrasana.com/about-us",
       description: "Home",
-      wordPressPostModules: [
-        generateDummyCTAModule(),
-        generateDummyInfoHeaderModule()
-      ]
+      wordPressPostModules: [generateDummyCTAModule(), generateDummyInfoHeaderModule()]
     } as PageModel,
     {
       id: uuid(),
       route: "https://terrasana.com/playground",
       description: "Test Page for building modules",
-<<<<<<< HEAD
-      wordPressPostModules: [
-        generateDummyContactModule(),
-        generateDummyOurTeamModule()
-      ]
-=======
-      wordPressPostModules: [generateDummyInstagramWallModule()]
->>>>>>> dev_daniele
+      wordPressPostModules: [generateDummyContactModule(), generateDummyOurTeamModule()]
     } as PageModel
   ];
 
