76e91bde1ea34d979bfb4a9961faf192efbbb9e5
elyyps <ahmed@webbio.nl>
2019-04-26 15:25:47 +0200

    feat: product overview component

--- src/product-cards/product-card.service.ts
+++ src/product-cards/product-card.service.ts
@@ -6,15 +6,157 @@ import { generateDummyProductOverviewModule } from "src/model/product-overview";
 export class ProductCardService {
   private GenerateCardList(length: number) {
     let cardList = [];
-    for (let i = 0; i <= length; i++) {
-      cardList.push(generateDummyProductOverviewModule());
-    }
+    for (let i = 0; i <= length; i++) {}
     return cardList;
   }
 
   public getCards(filter?: string): ProductCardsFilterReturn {
     const TOTAL_ITEMS = 1;
-    const cardList = this.GenerateCardList(TOTAL_ITEMS);
+    // const cardList = this.GenerateCardList(TOTAL_ITEMS);
+    let cardList = [];
+
+    cardList.push(
+      {
+        link: "/",
+        title: "Noedels",
+        category: "Noedels",
+        image: "",
+        button: { title: "Bekijk alle 12" },
+        cardVariant: "card--category",
+        modifyClass: "card--modify"
+      },
+      {
+        link: "/",
+        title: "Zeewieren",
+        category: "Zeewieren",
+        image: "",
+        button: { title: "Bekijk alle 12" },
+        cardVariant: "card--category",
+        modifyClass: "card--modify"
+      },
+      {
+        link: "/",
+        title: "Paddenstolen en seitan",
+        category: "Paddenstolen en seitan",
+        image: "",
+        button: { title: "Bekijk alle 12" },
+        cardVariant: "card--category",
+        modifyClass: "card--modify"
+      },
+
+      {
+        link: "/",
+        title: "Title",
+        category: "Noedels",
+        image:
+          "https://s3-eu-west-1.amazonaws.com/viper-development-images/Terrasana/products/Noedels/1.png",
+        content: "Content",
+        cardVariant: "card--product",
+        modifyClass: ""
+      },
+      {
+        link: "/",
+        title: "Title",
+        category: "Noedels",
+        image:
+          "https://s3-eu-west-1.amazonaws.com/viper-development-images/Terrasana/products/Noedels/2.png",
+        content: "Content",
+        cardVariant: "card--product",
+        modifyClass: ""
+      },
+      {
+        link: "/",
+        title: "Title",
+        category: "Noedels",
+        image:
+          "https://s3-eu-west-1.amazonaws.com/viper-development-images/Terrasana/products/Noedels/3.jpg",
+        content: "Content",
+        cardVariant: "card--product",
+        modifyClass: ""
+      },
+      {
+        link: "/",
+        title: "Title",
+        category: "Noedels",
+        image:
+          "https://s3-eu-west-1.amazonaws.com/viper-development-images/Terrasana/products/Noedels/4.jpg",
+        content: "Content",
+        cardVariant: "card--product",
+        modifyClass: ""
+      },
+
+      {
+        link: "/",
+        title: "Title",
+        category: "Zeewieren",
+        image:
+          "https://s3-eu-west-1.amazonaws.com/viper-development-images/Terrasana/products/Zeewieren/1.jpg",
+        content: "Content",
+        cardVariant: "card--product",
+        modifyClass: ""
+      },
+      {
+        link: "/",
+        title: "Title",
+        category: "Zeewieren",
+        image:
+          "https://s3-eu-west-1.amazonaws.com/viper-development-images/Terrasana/products/Zeewieren/2.jpg",
+        content: "Content",
+        cardVariant: "card--product",
+        modifyClass: ""
+      },
+      {
+        link: "/",
+        title: "Title",
+        category: "Zeewieren",
+        image:
+          "https://s3-eu-west-1.amazonaws.com/viper-development-images/Terrasana/products/Zeewieren/3.jpg",
+        content: "Content",
+        cardVariant: "card--product",
+        modifyClass: ""
+      },
+      {
+        link: "/",
+        title: "Title",
+        category: "Zeewieren",
+        image:
+          "https://s3-eu-west-1.amazonaws.com/viper-development-images/Terrasana/products/Zeewieren/4.jpg",
+        content: "Content",
+        cardVariant: "card--product",
+        modifyClass: ""
+      },
+
+      {
+        link: "/",
+        title: "Title",
+        category: "Paddenstolen en seitan",
+        image:
+          "https://s3-eu-west-1.amazonaws.com/viper-development-images/Terrasana/products/PaddenStoelen+en+seitan/1.png",
+        content: "Content",
+        cardVariant: "card--product",
+        modifyClass: ""
+      },
+      {
+        link: "/",
+        title: "Title",
+        category: "Paddenstolen en seitan",
+        image:
+          "https://s3-eu-west-1.amazonaws.com/viper-development-images/Terrasana/products/PaddenStoelen+en+seitan/2.jpg",
+        content: "Content",
+        cardVariant: "card--product",
+        modifyClass: ""
+      },
+      {
+        link: "/",
+        title: "Title",
+        category: "Paddenstolen en seitan",
+        image:
+          "https://s3-eu-west-1.amazonaws.com/viper-development-images/Terrasana/products/PaddenStoelen+en+seitan/3.jpg",
+        content: "Content",
+        cardVariant: "card--product",
+        modifyClass: ""
+      }
+    );
 
     let totalItems = TOTAL_ITEMS;
     if (filter) {
