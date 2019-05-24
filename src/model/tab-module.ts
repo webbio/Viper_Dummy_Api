d5f451ff9a5be36b0ab3391e70aec0b6a5e3b298
elyyps <ahmed@webbio.nl>
2019-05-24 14:11:40 +0200

    feat: added tab component

--- src/model/tab-module.ts
+++ src/model/tab-module.ts
@@ -2,10 +2,16 @@ import { ApiModelProperty } from '@nestjs/swagger';
 import { WordPressPostModule } from './wordpress-post-module';
 import { TabsHeader } from './tabs-header';
 import * as uuid from 'uuid';
+import {
+  SalePointModule,
+  generateDummySalePointModule,
+} from './sale-point-module';
 
 export class TabsModule extends WordPressPostModule {
   @ApiModelProperty({ type: [TabsHeader] })
   public tabs: TabsHeader[];
+  @ApiModelProperty({ type: SalePointModule })
+  public salePoints: SalePointModule;
 }
 
 export const generateDummyTabs = (): TabsModule => {
@@ -16,6 +22,7 @@ export const generateDummyTabs = (): TabsModule => {
       { title: 'Verkooppunten', moduleTab: 'SalePointComponent' },
       { title: 'B2B gegevens', moduleTab: '' },
     ],
+    salePoints: generateDummySalePointModule(),
     id: uuid(),
     topMargin: '',
     bottomMargin: '',
