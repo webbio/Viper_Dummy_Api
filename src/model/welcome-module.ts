f352888af8586e0fc51c6a8d6a7271468cc54ff0
Daniele <daniele@Webbios-MacBook-Pro.local>
2019-05-24 14:31:17 +0200

    added curve to welcome module

--- src/model/welcome-module.ts
+++ src/model/welcome-module.ts
@@ -38,6 +38,16 @@ export class WelcomeModule extends WordPressPostModule {
 export const generateDummyWelcomeModule = (): WelcomeModule => {
   return {
     id: uuid(),
+    background: {
+      backgroundPattern:
+        'https://s3-eu-west-1.amazonaws.com/viper-development-images/Terrasana/pattern-food.png',
+    },
+    curve: {
+      curveBackground: {
+        backgroundColour: '#fff',
+      },
+      decorCurveVerticalAlignment: 'bottom',
+    },
     name: 'WelcomeModule',
     topMargin: '0px',
     bottomMargin: '0px',
