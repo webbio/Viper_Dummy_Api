dbe7cab09a376f72563116b82724ea8cc71b1265
Tom Klein <tom@webbio.nl>
2019-06-20 16:53:12 +0200

    fixed spelling error

--- src/model/related-links.ts
+++ src/model/related-links.ts
@@ -1,5 +1,5 @@
-import { ApiModelProperty } from '@nestjs/swagger';
-import { Link } from './link';
+import { ApiModelProperty } from "@nestjs/swagger";
+import { Link } from "./link";
 
 export class RelatedLinks {
   @ApiModelProperty()
@@ -12,26 +12,23 @@ export class RelatedLinks {
 
 export const generateDummyRelatedLinks = (): RelatedLinks[] => [
   {
-    title: 'Onze producte n',
-    content: 'Bekijk onze pure & overheerlijke producten!',
-    button: { content: 'Bekijk alle producten', route: '/' },
+    title: "Onze producten",
+    content: "Bekijk onze pure & overheerlijke producten!",
+    button: { content: "Bekijk alle producten", route: "/" }
   },
   {
-    title: 'Heerlijke recepten',
-    content:
-      'Bekijk onze verrukkelijke recepten. Natuurlijk gemaakt met Terrasana producten.',
-    button: { content: 'Bekijk alle recepten', route: '/' },
+    title: "Heerlijke recepten",
+    content: "Bekijk onze verrukkelijke recepten. Natuurlijk gemaakt met Terrasana producten.",
+    button: { content: "Bekijk alle recepten", route: "/" }
   },
   {
-    title: 'Over ons',
-    content:
-      'Lees ons verhaal en zie wie er samen hard werken aan een bio-wereld!',
-    button: { content: 'Lees het verhaal', route: '/' },
+    title: "Over ons",
+    content: "Lees ons verhaal en zie wie er samen hard werken aan een bio-wereld!",
+    button: { content: "Lees het verhaal", route: "/" }
   },
   {
-    title: 'DOWNLOADS',
-    content:
-      'Bent u een verkoper van Terrasana producten? Download hier alle productinformatie.',
-    button: { content: 'Alle downloads', route: '/' },
-  },
+    title: "DOWNLOADS",
+    content: "Bent u een verkoper van Terrasana producten? Download hier alle productinformatie.",
+    button: { content: "Alle downloads", route: "/" }
+  }
 ];
